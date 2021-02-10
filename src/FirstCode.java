0cec194783cea163afbe62de8db3253a6dd93af8
Aditya Tiwari <atiwa007@fiu.edu>
2021-02-09 23:30:39 -0600

    Testing Github  Connection

--- src/FirstCode.java
+++ src/FirstCode.java
@@ -2,13 +2,6 @@
 public class FirstCode {	
 	public static void main(String args[])
 	{
-		int num=5;
-		float dec=6.8f;
-		long l=7l;
-		double d=9;
-		short s=9;
-		
-		System.out.println(num);
-		System.out.print(dec);
+		System.out.println("Hello Java World");
 	}
 }
--- src/Pattern.java
+++ src/Pattern.java
@@ -1,5 +1,5 @@
 
-public class DemoArithematic {
+public class Pattern {
 	public static void main(String args[])
 	{	
 		for(int i=1;i<7;i++)
