8d96d1dec32451a46311805a6b1d5eb80a43189e
Aditya Tiwari <atiwa007@fiu.edu>
2021-02-12 16:14:26 -0600

    Inheritance Demo

---
+++ src/InheritanceDemo.java
@@ -0,0 +1,39 @@
+/* In java we have multi-level inheritance and single level inheritance
+ * We don't have multiple inheritance
+ */
+
+class Calculator
+{
+	public int Sum(int i, int j)
+	{
+		return i+j;
+	}
+}
+
+class AdvCalculator extends Calculator //will have two methods add() from Calculator and sub
+{
+	public int Sub(int i, int j)
+	{
+		return i-j;
+	}
+}
+
+class VeryAdvCalculator extends AdvCalculator //will have 3 methods add(),sub() from AdvCalcualtor and mul
+{
+	public int Mul(int i, int j)
+	{
+		return i*j;
+	}
+}
+
+public class InheritanceDemo {
+
+	public static void main(String[] args) {
+		// Create objects using inheritance
+		VeryAdvCalculator obj = new VeryAdvCalculator();
+		System.out.println(obj.Sum(4, 3));
+		System.out.println(obj.Sub(4, 3));
+		System.out.println(obj.Mul(4, 3));
+	}
+
+}
